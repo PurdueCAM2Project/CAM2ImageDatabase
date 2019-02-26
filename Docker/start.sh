34e43c7b05809ff1bd4fcd6e808570502c387f58
LING ZHANG <junelena1313@gmail.com>
2019-02-26 14:51:03 -0500

    Update start.sh
    remove kill

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 docker-compose up -d
-#run the next line incase of fresh install only.
+#run the next two line incase of fresh install only.
 #./lvtctl.sh ApplySchema -sql "$(cat create_test_table.sql)" test_keyspace
-#docker-compose kill
-#docker-compose up -d
+# docker-compose up -d
 ./client.sh
 xdg-open https://localhost:20189
