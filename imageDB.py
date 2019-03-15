5d8b664e961890db627b278501827c720fd9814b
Lakshya <lgoyal@purdue.edu>
2019-03-14 23:14:03 -0500

    Added comparison for missing files in folder

--- imageDB.py
+++ imageDB.py
@@ -4,7 +4,7 @@ This file contains the main functionalities of the imageDB system.
 
 ----- Table Initialization -----
 
-1. Initialize camera table 
+1. Initialize camera table
 2. Initialize image table
 3. Initialize feature table
 4. Initialize image-feature relation table
@@ -51,7 +51,7 @@ class ImageDB:
 
         self.vitess = VitessConn()
         # TODO: Haoran add a line for Mino connection
-    
+
     def init_tables(self):
         self.vitess.createCameraTable()
         self.vitess.createFeatureTable()
@@ -71,15 +71,24 @@ class ImageDB:
         elif csv_header == required_header:
             return
         elif len(csv_header) < len(required_header):
-            raise ValueError("File is missing required column.") 
+            raise ValueError("File is missing required column.")
         elif len(csv_header) > len(required_header):
             raise ValueError("File exceeds the expected number of columns.")
         else:
             raise ValueError('File does not having correct header.')
 
-    # TODO: the csv file integrity check incorporate with Lakshya's code
+    # TODO: The csv file integrity check incorporate with Lakshya's code
+    # TODO: Compare function needs the path to folder containing image files.
+    #       This parameter needs to be added to the read_data function
+    # TODO: Still need to check if files in folder exist in CSV. Would this be a separate function?
     @classmethod
-    def read_data(csv_file, required_header, data_format):
+    def read_data(csv_file, required_header, data_format, folder_path):
+        # Get the list of files in the folder containing the images
+        files_in_folder = os.listdir(folder_path)
+        # List of names of files that are missing in either the CSV or folder
+        missing_from_CSV = []
+        missing_from_folder = []
+
         header = []
         if data_format == 'dict':
             items = {}
@@ -87,23 +96,36 @@ class ImageDB:
             items = []
         try:
             with open(csv_file, 'rb') as csvfile:
+                # reader is the list of rows from the CSV
                 reader = csv.reader(csvfile, delimiter=',')
 
-                # check if it has the corret header
+                # Check if it has the corret header
                 ImageDB.check_header(reader[0], required_header)
                 header = reader[0]
                 reader = reader[1:]
 
-                for i in reader:
-                    if len(i) != len(required_header):
+                for row in reader:
+                    if len(row) != len(required_header):
                         raise ValueError("File content column does not match header.")
                     if data_format == 'tuple':
-                        items.append(tuple(i))
+                        items.append(tuple(row))
                     elif data_format == 'list':
-                        items.append(i)
+                        # If the data_format is a 'list' then it means that image files are being
+                        # inserted. Comparison takes place here to check for file existance
+                        items.append(row)
+
+                        # Check if file name exists in the folder
+                        if row[0] not in files_in_folder:
+                            missing_from_folder.append(row[0])
+
+                        # If there are missing files, print them
+                        if missing_from_folder:
+                            print("The following files are missing from the folder:")
+                            print(missingFolder)
+
                     elif data_format == 'dict':
                         # key: image file name; value: entire row (list)
-                        items[i[0]] = i
+                        items[row[0]] = row
                     else:
                         print('Must specify to read as dict, list of lists or tuples.')
         except IOError as e:
@@ -130,7 +152,7 @@ class ImageDB:
         except Exception as e2:
             print(e2)
 
-    # this function should read image and image feature file, 
+    # this function should read image and image feature file,
     def insert_image(self, image_csv, image_feature_csv=None):
 
         # image_list is a list of list, each element is a row in csv
@@ -154,13 +176,13 @@ class ImageDB:
                 if f_id is not None:
                     relation_header[i] = f_id
                 else:
-                    # generate feature id, insert new feature 
+                    # generate feature id, insert new feature
                     f_id = uuid.uuid1()
                     self.vitess.insertFeature((f_id, relation_header[i]))
                     relation_header[i] = f_id
 
             # insert image with metadata and feature inside the database one-by-one
-            
+
             for i in len(image_list):
 
                 # image_list[i] is the i th row of image metadata
