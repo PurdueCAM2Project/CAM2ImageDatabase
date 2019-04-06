14eb748e6ff7be17ce5ba2bb4b345a979321529f
Ayushi Gupta <ayushi.pixie@gmail.com>
2019-04-06 19:49:55 -0400

    Converted camera metadata script to function that accepts image csv file

--- camera_metadata_script.py
+++ camera_metadata_script.py
@@ -1,51 +1,52 @@
 import CAM2CameraDatabaseAPIClient.client as cam2
 import csv
-from CAM2CameraDatabaseAPIClient.client import Client
-from CAM2ImageArchiver.CAM2ImageArchiver import CAM2ImageArchiver
 
+clientID = '%s' %(cam2.Client.clientID) # placeholder
+clientSecret = '%s' %(cam2.Client.clientSecret) # placeholder
 
-clientID = '%s' %(cam2.Client.clientID)
-clientSecret = '%s' %(cam2.Client.clientSecret)
 db = cam2.Client(clientID, clientSecret)
-
-archiver = CAM2ImageArchiver(num_processes=1)
-
-cameras = db.search_camera(city="NY", offset=0)
-
-#To search camera by all camera ids:
-# i = 0
-# while len(cameras) != 0:
-#     cameras = db.search_camera(offset=i)
-#     i += 100
-
-IDList = []
-for camera in cameras:
-    IDList.append(camera['cameraID'])
 
 cameras1 = []
 
-for camId in IDList:
-    cameras1.append(db.camera_by_id(camId))
 
-archiver.archive(cameras1, duration=1, interval=1)
+def camera_metadata(file):
+    try:
+        with open(file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count != 0:
+                    cameras1.append(db.camera_by_id(row[1]))
+                line_count += 1
+    except Exception as e:
+        if file:
+            file.close()
+        print(e)
 
-output_file = open('file2_name.csv', 'w')
+    try:
+        output_file = open('file2_name.csv', 'w')
 
-fields = ['Camera_ID', 'camera_country', 'camera_state', 'camera_city', 'camera_latitude', 'camera_longitude', 'resolution_width', 'resolution_height']
-writer = csv.writer(output_file)
-writer.writerow(fields)
+        fields = ['Camera_ID', 'camera_country', 'camera_state', 'camera_city', 'camera_latitude', 'camera_longitude', 'resolution_width', 'resolution_height']
+        writer = csv.writer(output_file)
+        writer.writerow(fields)
 
-for camera in cameras1:
-    Camera_ID = camera['cameraID']
-    camera_city = camera['city']
-    camera_country = camera['country']
-    camera_state = camera['state']
-    camera_longitude = camera['camera_longitude']
-    camera_latitude = camera['camera_latitude']
-    camera_rwidth = camera['resolution_width']
-    camera_rheight = camera['resolution_height']
-    writer.writerow([Camera_ID, camera_country, camera_state, camera_city, camera_latitude, camera_longitude, camera_rwidth, camera_rheight])
+        for camera in cameras1:
+            camera_ID = camera['cameraID']
+            camera_city = camera['city']
+            camera_country = camera['country']
+            camera_state = camera['state']
+            camera_longitude = camera['longitude']
+            camera_latitude = camera['latitude']
+            camera_rwidth = camera['resolution_width']
+            camera_rheight = camera['resolution_height']
+            writer.writerow([camera_ID, camera_country, camera_state, camera_city, camera_latitude, camera_longitude, camera_rwidth, camera_rheight])
+
+    except Exception as e:
+        if output_file:
+            output_file.close()
+        print(e)
+
+    output_file.close()
 
 
-
-
+camera_metadata('file_name.csv')
