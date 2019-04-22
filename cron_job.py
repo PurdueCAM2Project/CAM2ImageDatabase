9782666051079e8433d7491460c94f07d42f0e24
Ayushi Gupta <ayushi.pixie@gmail.com>
2019-04-21 23:15:27 -0400

    Removed debugging print statements

--- cron_job.py
+++ cron_job.py
@@ -3,16 +3,8 @@ from crontab import CronTab
 cron = CronTab()
 
 job = cron.new(command='python image_retrieval.py')
-job.minute.every(1)  # can change according to requirement (hours, week, month, etc.)
+job.every(1).month  # can change according to requirement (hours, week, month, etc.)
 
 cron.write()
 print("Job created")
 
-for job in cron:
-    print(job)
-
-cron.remove(job)
-print("Job removed")
-
-for job in cron:
-    print(job)
